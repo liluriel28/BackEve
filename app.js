84db98b90ab809cfa8e9b8aba25eba8349efa377
liluriel28 <a3521110073@alumno.uttehuacan.edu.mx>
2024-12-03 16:14:39 -0600

    prueba

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import morgan from 'morgan';
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,19 +7,23 @@ import cors from "cors";
 
 const app = express();
 
-
+// Configuración de CORS
 app.use(
-    cors({
-      credentials: true,
-      origin: 'http://localhost:5173',
-    })
-  );
+  cors({
+    credentials: true, // Permite el manejo de cookies
+    origin: ['http://localhost:5173', 'https://back-eve.vercel.app'], // Permite ambos orígenes
+  })
+);
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
+// Rutas
 app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
+
+// Ruta para manejar las solicitudes OPTIONS (preflight)
+app.options('*', cors()); // Permite solicitudes OPTIONS en todas las rutas
 
 export default app;
