05f746f9188b87e90abf3f6e7f3c920fd9b12f80
liluriel28 <a3521110073@alumno.uttehuacan.edu.mx>
2024-12-03 16:19:48 -0600

    hola

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ app.use(
   cors({
     credentials: true, // Permite el manejo de cookies
     origin: ['http://localhost:5173', 'https://back-eve.vercel.app'], // Permite ambos orígenes
+    methods: ['GET', 'POST', 'OPTIONS'], // Asegúrate de permitir todos los métodos que tu API necesite
+    allowedHeaders: ['Content-Type', 'Authorization'], // Asegúrate de permitir los encabezados que tu API necesita
   })
 );
+
+// Manejo de solicitudes OPTIONS
+app.options('*', cors()); // Permite solicitudes OPTIONS en todas las rutas
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -22,8 +27,5 @@ app.use(cookieParser());
 // Rutas
 app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
-
-// Ruta para manejar las solicitudes OPTIONS (preflight)
-app.options('*', cors()); // Permite solicitudes OPTIONS en todas las rutas
 
 export default app;
