eb3f0e44102ff93619db01d59e52d11082eea429
liluriel28 <a3521110073@alumno.uttehuacan.edu.mx>
2024-12-03 17:08:11 -0600

    hola

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     credentials: true, // Permite el manejo de cookies
-    origin: ['http://localhost:5173', 'https://back-eve.vercel.app'], // Permite ambos orígenes
+    origin: ['http://localhost:5173', 'https://back-eve.vercel.app', 'https://cc67-189-190-165-116.ngrok-free.app/ '], // Permite ambos orígenes
     methods: ['GET', 'POST', 'OPTIONS'], // Asegúrate de permitir todos los métodos que tu API necesite
     allowedHeaders: ['Content-Type', 'Authorization'], // Asegúrate de permitir los encabezados que tu API necesita
   })
